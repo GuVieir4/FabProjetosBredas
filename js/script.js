a6dbeaf45d0506ada9752d1817dda07f7e345ee7
Gustavo Henrique <160288170+GuVieir4@users.noreply.github.com>
2025-04-21 19:02:01 -0300

    Página de Login

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const btnClose = document.getElementById('icon-close');
-const promoRegister = document.getElementById('promo-register')
+const promoRegister = document.getElementById('promo-register');
+const btnLogin = document.getElementById('login');
+
 
 btnClose.addEventListener("click", () => {
     promoRegister.classList.add('active')
