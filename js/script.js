d317d5c8cf0740e154a8be6b154863dd89ee8224
GuVieir4 <gurednike@gmail.com>
2025-04-22 00:57:37 -0300

    Dropdown de Login e Ajustes na Responsividade

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+// Pessoal, manter os scripts separados por comentários/html por enquanto. Se futuramente o tamanho do JavaScript aumentar, nós criamos outros arquivos.
+
+// Script do Index
+
 const btnClose = document.getElementById('icon-close');
 const promoRegister = document.getElementById('promo-register');
 const btnLogin = document.getElementById('login');
@@ -6,3 +10,19 @@ const btnLogin = document.getElementById('login');
 btnClose.addEventListener("click", () => {
     promoRegister.classList.add('active')
 })
+
+// Script da Página de Login
+
+const userIcon = document.getElementById("user-icon");
+const dropdownMenu = document.getElementById("dropdown-menu");
+
+userIcon.addEventListener("click", (event) => {
+    event.preventDefault();
+    dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+});
+
+document.addEventListener("click", (event) => {
+    if (!userIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.style.display = "none";
+    }
+});
