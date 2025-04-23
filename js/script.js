7adee7bf12d8ab0a3d48df069490060929bf0aab
Gustavo Henrique <160288170+GuVieir4@users.noreply.github.com>
2025-04-22 21:39:47 -0300

    Validação de Senhas no Cadastro

--- js/script.js
+++ js/script.js
@@ -1,28 +1,51 @@
-// Pessoal, manter os scripts separados por comentários/html por enquanto. Se futuramente o tamanho do JavaScript aumentar, nós criamos outros arquivos.
+document.addEventListener("DOMContentLoaded", () => {
 
-// Index > Fechar a promoção de cadastro
-
-const btnClose = document.getElementById('icon-close');
-const promoRegister = document.getElementById('promo-register');
-const btnLogin = document.getElementById('login');
-
-
-btnClose.addEventListener("click", () => {
-    promoRegister.classList.add('active')
-})
-
-// Index > Dropdown
-
-const userIcon = document.getElementById("user-icon");
-const dropdownMenu = document.getElementById("dropdown-menu");
-
-userIcon.addEventListener("click", (event) => {
-    event.preventDefault();
-    dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
-});
-
-document.addEventListener("click", (event) => {
-    if (!userIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
-        dropdownMenu.style.display = "none";
-    }
-});
+    // Pessoal, manter os scripts separados por comentários/html por enquanto. Se futuramente o tamanho do JavaScript aumentar, nós criamos outros arquivos.
+    
+    // Index > Fechar a promoção de cadastro
+    
+    const btnClose = document.getElementById('icon-close');
+    const promoRegister = document.getElementById('promo-register');
+    const btnLogin = document.getElementById('login');
+    
+        if (btnClose && promoRegister) {
+            btnClose.addEventListener("click", () => {
+                promoRegister.classList.add('active')
+            })
+        }
+    
+    // Index > Dropdown
+    
+    const userIcon = document.getElementById("user-icon");
+    const dropdownMenu = document.getElementById("dropdown-menu");
+        
+        if (userIcon && dropdownMenu) {
+            userIcon.addEventListener("click", (event) => {
+                event.preventDefault();
+                dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+            });
+    
+            document.addEventListener("click", (event) => {
+                if (!userIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
+                    dropdownMenu.style.display = "none";
+                }
+            });
+        }
+    
+    // Register > Confirmar senha
+    
+    const form = document.getElementById("formulario");
+    const senha = document.getElementById("password");
+    const confirmarSenha = document.getElementById("confirm-password");
+    const mensagemDeErro = document.getElementById("mensagemDeErro");
+    const alertaDeErro = document.getElementById('alertErro');
+    
+            form.addEventListener("submit", function (event) {
+                if (senha.value !== confirmarSenha.value) {
+                    event.preventDefault();
+                    alertaDeErro.classList.add('active')
+                }
+            });
+    
+    });
+    
